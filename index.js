c4708e9ae3fb13c64ed38ffd08c83e9293a63871
tiefengeist <sahirnambiar@gmail.com>
2019-03-18 14:46:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.soup = soup;
+    this.salad = salad;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.entree = entree;
+    this.salad = salad;
+    this.soup = soup;
+    this._dessert = dessert;
+  }
+}
